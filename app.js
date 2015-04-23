0be33a9efcbf259b9eee6800d20e453a742f9a0c
unknown <basavaraj.teli@PDC2A-429SX73.GDNINDIA.COM>
2015-04-23 15:57:04 +0530

    AuthGithub assignment added

---
+++ app.js
@@ -0,0 +1,123 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  routes = require('./routes');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+var GITHUB_CLIENT_ID = "6f5d091f7d886128263d";
+var GITHUB_CLIENT_SECRET = "6e77026f8220a2c644128d73c81fde7574f184e1";
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the GitHubStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and GitHub
+//   profile), and invoke a callback with a user object.
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.set('view options', {
+    layout: false
+  });
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());    
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());    
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+/*app.get('/api/posts', api.posts);
+
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);*/
+/*github authentication*/
+app.get('/auth/github',
+  passport.authenticate('github'),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
+
+/*If authentication fails, the user will be redirected back to the
+    login page.  Otherwise, will redirect the user to the home page.
+*/    
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+   app.get('/account', function(req, res){
+     if (req.isAuthenticated()) { 
+       res.send({user : req.user}); 
+     }else{
+       res.redirect('/login');
+     }
+   });
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+/*If the request is authenticated the request will proceed.  Otherwise, the user will be redirected to the
+   login page.*/
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+
+var app = angular.module('app',[]);
+app.config(function($locationProvider, $routeProvider) {
+  // $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/', { templateUrl: 'partials/index', controller: 'ctrl' })
+    .when('/about', { templateUrl: 'partials/about', controller: 'ctrl' })
+    .otherwise({redirectTo:'/'});
+});
+ 
+app.controller('ctrl', function($scope,$http){
+    $http.get('/account').
+    success(function(data) {
+      $scope.userData = data.user;
+    });
+$scope.message='Welcome! Please log in.';
+
+});
